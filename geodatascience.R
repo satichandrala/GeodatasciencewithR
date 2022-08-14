a6a90e01f47d3138da43fdad1f7d83417dd9bffb
Purna <satichandrala@gmail.com>
2022-08-14 02:36:52 +0200

    introducing using geodata with r

---
+++ geodatascience.R
@@ -0,0 +1,8 @@
+library(sf)
+install.packages("terra")
+install.packages("spData")
+install.packages("terra")
+install.packages("spDataLarge")
+vignette(package = "sf") # see which vignettes are available
+vignette("sf1")          # an introduction to the package
+setwd("C:/Users/satic/geodatasciencewithR")
